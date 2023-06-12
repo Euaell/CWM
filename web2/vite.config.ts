195ea35a0e9839b9c0484a77c5c70984a9f834be
euael <euaelmeko@gmail.com>
2023-06-12 21:29:08 +0300

    debbuging2

--- web2/vite.config.ts
+++ web2/vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dns from 'dns'
+
+dns.setDefaultResultOrder('verbatim')
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode, ssrBuild}) => {
 	return {
 		plugins: [react()],
+		server: {
+			host: 'localhost',
+			port: 3017
+		}
 	}
 })
